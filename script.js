70273baab3c658716edcd0753d01a5e0af14cbaf
lisapichpich <lisapichon72@gmail.com>
2026-01-14 15:10:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function afficherTexte() {
+            const texte = document.getElementById("texte");
+            texte.style.display =
+            texte.style.display === "none" ? "block" : "none";
+}
